bdd0f0e85dd921d184ca1a499bb98a34ab3a9433
wangchao <wang153723482@gmail.com>
2021-07-01 20:15:45 +0800

    init

---
+++ src/core/TEST.java
@@ -0,0 +1,12 @@
+import java.util.Date;
+
+/**
+ * Created by wangc on 2020/5/13.
+ *
+ * @author wangc
+ */
+public class TEST {
+    public static void main(String[] args){
+        System.out.println( new Date().getTime() );
+    }
+}
